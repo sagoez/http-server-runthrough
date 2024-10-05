562c9d49bf2e473e16adedc92d9fc31d54bf46c9
Samuel Gomez <samgj_18@hotmail.com>
2024-10-05 15:23:13 +0100

    task: respond with body

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-// Uncomment this block to pass the first stage
 use itertools::Itertools;
+use nom::AsBytes;
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
@@ -17,18 +17,11 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                //TODO: Temporarily making it all optional
-                let bytes = read_bytes(&mut stream);
-                let string = bytes_to_str(bytes).ok();
-                let request = parse_req(string);
-
-                if let Some(request) = request {
-                    if request.method.path() == "/" {
-                        stream.write_all(OK.as_bytes()).ok();
-                    } else {
-                        stream.write_all(NOT_FOUND.as_bytes()).ok();
-                    }
+                match process_req(&mut stream) {
+                    Some(r) => r.stream.write_all(&r.response.bytes).ok(),
+                    None => Some(()),
                 };
+                // process_req(&mut stream).and_then(|r| stream.write_all(NOT_FOUND.as_bytes()).ok());
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -37,9 +30,71 @@ fn main() {
     }
 }
 
-pub struct Request {
+pub struct HTTP<'a> {
+    // request: Request,
+    response: Response,
+    stream: &'a mut TcpStream,
+}
+
+fn process_req(stream: &mut TcpStream) -> Option<HTTP<'_>> {
+    let bytes = read_bytes(stream);
+    let string = bytes_to_str(bytes).ok();
+    let request = parse_req(string);
+
+    if let Some(request) = request {
+        match (request.path.path.as_str(), &request.method) {
+            ("/", Method::Get) => Some(HTTP {
+                // request,
+                response: Response {
+                    content_lengh: 0,
+                    bytes: OK.as_bytes().to_vec(),
+                },
+                stream,
+            }),
+            ("echo", Method::Get) => {
+                let body = request.path.params.join("");
+                let len = body.len();
+
+                // TODO: Build response builder
+                let res = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    len, body
+                );
+                Some(HTTP {
+                    // request,
+                    response: Response {
+                        content_lengh: len,
+                        bytes: res.as_bytes().to_vec(),
+                    },
+                    stream,
+                })
+            }
+            _ => Some(HTTP {
+                // request,
+                response: Response {
+                    content_lengh: 0,
+                    bytes: NOT_FOUND.as_bytes().to_vec(),
+                },
+                stream,
+            }),
+        }
+    } else {
+        None
+    }
+}
+
+pub struct Response {
+    content_lengh: usize,
+    bytes: Vec<u8>,
+}
+
+#[derive(Debug)]
+struct Request {
     method: Method,
-    headers: Vec<String>,
+    path: Path,
+    #[allow(dead_code)]
+    header: Vec<String>,
+    body: Vec<u8>,
 }
 
 // TODO: Split headers and take body into account
@@ -47,15 +102,20 @@ fn parse_req(str: Option<String>) -> Option<Request> {
     match str {
         Some(str) => match str.split("\r\n").collect_vec().split_first() {
             Some((first, elements)) => {
-                let method = Method::from_static(first);
-                method.and_then(|method| {
+                let request_parts = RequestPart::from_static(first);
+                request_parts.and_then(|rp| {
                     let rest = elements.split(|s| s == &"\r\n\r\n").collect_vec();
-
                     let rest = rest.split_first();
 
-                    rest.map(|(headers, _body)| Request {
-                        method,
-                        headers: headers.iter().copied().map(|s| s.to_owned()).collect(),
+                    rest.map(|(headers, body)| {
+                        let header = headers.iter().copied().map(|s| s.to_owned()).collect();
+
+                        Request {
+                            method: rp.method,
+                            path: rp.path,
+                            header,
+                            body: flatten_to_bytes(body),
+                        }
                     })
                 })
             }
@@ -92,35 +152,64 @@ fn read_bytes(stream: &mut TcpStream) -> Vec<u8> {
     buffer
 }
 
-pub enum Method {
-    Get {
-        path: String,
-        http: String, // Either V1, or V2
-    },
+struct RequestPart {
+    method: Method,
+    path: Path,
+}
+
+#[derive(PartialEq, Debug)]
+enum Method {
+    Get,
 }
 
 impl Method {
-    pub fn path(&self) -> String {
-        match self {
-            Method::Get { path, .. } => path.into(),
+    fn from_static(str: &str) -> Option<Method> {
+        match str {
+            "GET" => Some(Method::Get),
+            _ => None,
         }
     }
+}
 
-    pub fn from_static(str: &str) -> Option<Method> {
-        let splitted = str.split(" ").collect_vec();
-        // Extract URL path
-        if splitted.len() != 3 {
-            None
+impl RequestPart {
+    fn from_static(str: &str) -> Option<RequestPart> {
+        let request_parts = str.split(" ").collect_vec();
+
+        if let [method, path, _http] = &request_parts[..] {
+            let elements = path
+                .trim()
+                .split_terminator("/")
+                .filter(|s| !s.is_empty())
+                .collect_vec();
+
+            let path = match elements.split_first() {
+                Some((path, params)) => Path {
+                    path: path.to_string(),
+                    params: params.iter().copied().map(|s| s.to_owned()).collect_vec(),
+                },
+                None => Path {
+                    path: path.to_string(),
+                    params: vec![],
+                },
+            };
+
+            Method::from_static(method).map(|method| RequestPart { method, path })
         } else {
-            // TODO: Generalize this
-            if splitted[0] == "GET" {
-                Some(Method::Get {
-                    path: splitted[1].to_owned(),
-                    http: splitted[2].to_owned(),
-                })
-            } else {
-                None
-            }
+            None
         }
     }
+}
+
+#[derive(Debug)]
+pub struct Path {
+    path: String,
+    params: Vec<String>,
+}
+
+// TODO: Pretty sure this is wrong
+fn flatten_to_bytes(body: &[&[&str]]) -> Vec<u8> {
+    body.iter()
+        .flat_map(|inner| inner.iter().flat_map(|&s| s.as_bytes()))
+        .copied()
+        .collect()
 }
