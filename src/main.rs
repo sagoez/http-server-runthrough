79e44407519b5354b49d04a0556ef00048f3a7e6
Samuel Gomez <samgj_18@hotmail.com>
2024-10-05 11:05:07 +0100

    Return a 200

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,12 +14,35 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).ok();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn read_bytes(stream: &mut TcpStream) -> Vec<u8> {
+    let mut buffer = Vec::new();
+    let mut bytes_read = 0;
+    loop {
+        let mut buf = [0; 1024];
+        match stream.read(&mut buf) {
+            Ok(n) => {
+                bytes_read += n;
+                buffer.extend_from_slice(&buf[..n]);
+                if bytes_read == buffer.len() {
+                    break;
+                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
+                break;
+            }
+        }
+    }
+
+    buffer
 }
